d80b3cedd209cbc57d7632ab07db548344574624
AzharDotCoder <azar123khan456@gmail.com>
2024-05-08 13:50:28 +0530

    created files and updated codes

---
+++ dashboard.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Online voting system - Dashboard</title>
+</head>
+<body>
+    <button>Back</button>
+    <button>Logout</button>
+    <h1>Online Voting System</h1> <hr>    
+
+
+</body>
+</html>
